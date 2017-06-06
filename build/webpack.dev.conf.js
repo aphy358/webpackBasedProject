9fb4e17a97452e40f2384934991f526866d5012a
aphy358 <358937572@qq.com>
2017-06-06 20:13:10 +0800

    0

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,9 +7,44 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function(name) {
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
     baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
 })
+
+var plugins = [];
+//***获取所有入口文件名
+var Entries = utils.getAllEntries()
+Entries.forEach((page) => {
+    var htmlPlugin = new HtmlWebpackPlugin({
+        filename: page + '.html',
+        template: 'template.html',
+        inject: true,
+        injectItem: ['manifest', 'common', page], //***新添加一个option选项
+        chunksSortMode: 'dependency'
+    })
+    plugins.push(htmlPlugin)
+})
+
+var otherPlugins = [
+    new webpack.DefinePlugin({
+        'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    // new HtmlWebpackPlugin({
+    //     filename: 'index.html',
+    //     template: 'template.html',
+    //     inject: true,
+    // }),
+    new FriendlyErrorsPlugin(),
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+    }),
+]
 
 module.exports = merge(baseWebpackConfig, {
     module: {
@@ -17,24 +52,5 @@ module.exports = merge(baseWebpackConfig, {
     },
     // cheap-module-eval-source-map is faster for development
     devtool: '#cheap-module-eval-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': config.dev.env
-        }),
-        // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        // https://github.com/ampedandwired/html-webpack-plugin
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'template.html',
-            inject: true,
-        }),
-        new FriendlyErrorsPlugin(),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        }),
-    ]
+    plugins: plugins.concat(otherPlugins)
 })
