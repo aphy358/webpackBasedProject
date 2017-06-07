82fce2055de6de13d9c71a9ad89c6efee4ffdfb7
Joker <chenhua385148231@126.com>
2017-06-07 09:58:08 +0800

    0

--- src/components/commonHeader/commonHeader.js
+++ src/components/commonHeader/commonHeader.js
@@ -1,7 +1,6 @@
 //分别引入相关的 css 和 html
 import './commonHeader.css'
 var html = require('./commonHeader.html')
-
 export function tmp() {
     return html;
 }
--- src/entries/index.js
+++ src/entries/index.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 require.ensure('~/commonHeader/commonHeader.js', function(data) {
     const commonHeaderjs = require('~/commonHeader/commonHeader.js')
-    $('#mainContent').html(commonHeaderjs.tmp())
+    $('#mainContent').html(commonHeaderjs.tmp());
+    $("title").html('首页')
 }, null, 'commonHeader');
