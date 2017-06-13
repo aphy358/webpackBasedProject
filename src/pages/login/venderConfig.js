98372a05128383676f5df494030a7a05aa41339a
Joker <chenhua385148231@126.com>
2017-06-13 10:21:28 +0800

    0

--- src/pages/login/venderConfig.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  img: {
-    fzg_logo: require('!!file-loader?name=static/img/[name].[ext]!../../static/img/fzg_logo.png'),  //注意后面这一段路径一定要写对，不然会报错
-    login_bg: require('!!file-loader?name=static/img/login/[name].[ext]!../../static/img/login/login_bg.jpg'),
-  },
-};
