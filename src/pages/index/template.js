35265d574d840cdc05dc9b25d7e30c62c7a99371
aphy358 <358937572@qq.com>
2017-06-14 21:20:10 +0800

    0

--- src/pages/index/template.js
+++ src/pages/index/template.js
@@ -1,6 +1,16 @@
+var banner = require('./content/banner.ejs');
+var search = require('./content/search.ejs');
+
+
+var staticConfig = require('../../static/staticConfig');
 var content = require('./content.ejs');
 var layout = require('../../layout/layout');
-var staticConfig = require('../../static/staticConfig');
 var pageTitle = '房掌柜 · 首页';
 
-module.exports = layout.run(pageTitle, content({staticConfig}));
+var params = {
+    staticConfig,
+    banner: banner({staticConfig}),     //千万记得参数是一个对象
+    search: search(),
+}
+
+module.exports = layout.run(pageTitle, content(params));
