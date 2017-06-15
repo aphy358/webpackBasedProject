76e9f96bf634d2f6532e7d0a6a63bb2a6838c50e
Joker <chenhua385148231@126.com>
2017-06-15 16:22:05 +0800

    0

--- src/pages/index/template.js
+++ src/pages/index/template.js
@@ -1,6 +1,8 @@
 var banner = require('./chunks/banner.ejs');
 var search = require('./chunks/search.ejs');
 var hotSales = require('./chunks/hotSales.ejs');
+var hotelRecommands = require('./chunks/hotelRecommands.ejs');
+var ticket = require('./chunks/ticket.ejs');
 
 
 var staticConfig = require('../../static/staticConfig');
@@ -10,9 +12,11 @@ var pageTitle = '房掌柜 · 首页';
 
 var params = {
     staticConfig,
-    banner: banner({staticConfig}),         //banner（千万记得参数是一个对象）
-    search: search(),                       //搜索框
-    hotSales: hotSales({staticConfig}),     //当季热销
+    banner: banner({staticConfig}),                     //banner（千万记得参数是一个对象）
+    search: search(),                                   //搜索框
+    hotSales: hotSales({staticConfig}),                 //当季热销
+    hotelRecommands: hotelRecommands({staticConfig}),   //国内酒店、国际酒店
+    ticket: ticket({staticConfig}),                     //景点门票
 }
 
 module.exports = layout.run(pageTitle, pageContent(params));
