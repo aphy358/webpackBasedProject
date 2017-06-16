74ff28e671e682719dcdb057c887ec6e8f657ce1
Joker <chenhua385148231@126.com>
2017-06-16 15:20:18 +0800

    1

---
+++ src/js/index/banner.js
@@ -0,0 +1,79 @@
+
+//首页 banner 区域相关 js
+
+var timeOut = null;
+
+module.exports = {
+
+    //判断是否是IE浏览器
+    isIE: function(){ return !!window.ActiveXObject || "ActiveXObject" in window; },
+
+    //如果是非IE浏览器，则异步加载swiper.js，并初始化swiper
+    loadSwiper: function(){
+        require.ensure(['swiper.jquery.min'], function(){
+            
+            require('swiper.jquery.min');
+
+            //IE6/7/8不兼容，IE9/10下勉强能用，但表现不佳。其他浏览器没测...
+            var swiper = new Swiper('.swiper-container', {
+                pagination: '.swiper-pagination',
+                paginationClickable: true,
+                autoplay: 5000,								//可选选项，自动滑动
+                autoplayDisableOnInteraction: false,		//轮播区域滑动、点击底部控制条不中断轮播
+            });
+        }, 'swiper');
+    },
+
+    //若果是IE浏览器，则另行处理，只为兼容...
+    swiperOnIE: function(){
+
+        var slideArr = $(".swiper-slide"),
+            paginationStr = '';
+
+        for(let i = 0; i < slideArr.length; i++){
+            paginationStr += '<span class="swiper-pagination-bullet"></span>';
+        }
+
+        if( paginationStr !== '' ){
+            $(".swiper-container").addClass('swiper-container-horizontal');
+            $(".swiper-pagination").append(paginationStr);
+            $($(".swiper-slide")[0]).addClass('swiper-slide-active');
+            $($(".swiper-pagination-bullet")[0]).addClass('swiper-pagination-bullet-active');
+
+            slideTimeOut();
+
+            initMouseEvent();
+        }
+        
+    },
+
+}
+
+function slideTimeOut(){
+
+    timeOut = setTimeout(function(){
+
+        var nextSlide = $(".swiper-slide-active").next('.swiper-slide');
+        $(".swiper-slide-active").removeClass('swiper-slide-active');
+        nextSlide.length > 0 ? nextSlide.addClass('swiper-slide-active')
+                             : $($(".swiper-slide")[0]).addClass('swiper-slide-active');
+
+        var nextBullet = $(".swiper-pagination-bullet-active").next('.swiper-pagination-bullet');
+        $(".swiper-pagination-bullet-active").removeClass('swiper-pagination-bullet-active');
+        nextBullet.length > 0 ? nextBullet.addClass('swiper-pagination-bullet-active')
+                              : $($(".swiper-pagination-bullet")[0]).addClass('swiper-pagination-bullet-active');
+
+        slideTimeOut();
+
+    }, 1000);
+}
+
+function initMouseEvent(){
+    $(".swiper-slide").on('mouseover', function(){
+        clearTimeout(timeOut);
+    });
+
+    $(".swiper-slide").on('mouseout', function(){
+        slideTimeOut();
+    });
+}
--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,20 +1,15 @@
-window.Promise = require('es6-promise');    //全局变量Promise，兼容ie
+//全局变量Promise，兼容ie
+window.Promise = require('es6-promise');
+
+//引入样式文件
 require('../../sass/header.scss');
 require('../../sass/footer.scss');
 require('../../static/fonts/iconfont.css');
 require('../../static/css/swiper.min.css');
 require('../../sass/index/index.scss');
 
-//异步加载 swiper 插件
-require.ensure(['swiper.jquery.min'], function(){
-    
-    require('swiper.jquery.min');
-    
-    //IE6/7/8不兼容，IE9/10下勉强能用，但表现不佳。后期如要改进，可能要重写该插件。
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        autoplay: 5000,								//可选选项，自动滑动
-        autoplayDisableOnInteraction: false,		//轮播区域滑动、点击底部控制条不中断轮播
-    });
-}, 'swiper');
+//引入js文件
+var banner = require('./banner.js');
+
+//处理轮播初始化
+banner.isIE() ? banner.swiperOnIE() : banner.loadSwiper();
