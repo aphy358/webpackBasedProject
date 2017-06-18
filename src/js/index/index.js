d1099db5a2707efaf60039a94be32d1a8c414545
aphy358 <358937572@qq.com>
2017-06-18 09:59:46 +0800

    0

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -8,8 +8,18 @@ require('../../static/fonts/iconfont.css');
 require('../../static/css/swiper.min.css');
 require('../../sass/index/index.scss');
 
+//引入模板文件
+const hotelRecommandsT = require('../../html/index/templates/hotelRecommands.T.ejs');
+
 //引入js文件
 const banner = require('./banner.js');
 
 //处理轮播初始化
 banner.isIE() ? banner.swiperOnIE() : banner.loadSwiper();
+
+setTimeout(function(){
+
+    var data = {cities: ['深圳','珠海','香港','东莞','上海','北京']};
+
+    $('#testT').html( hotelRecommandsT(data) );
+}, 10);
