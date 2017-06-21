fa0b0ed114ea4a0597434b846ed85a18c095f298
Joker <chenhua385148231@126.com>
2017-06-21 18:09:13 +0800

    0

--- config/api.js
+++ config/api.js
@@ -2,8 +2,17 @@
 
 //所有被代理的api
 
-let proxy_addr = 'http://localhost:8083';
+let proxy_addr = 'http://192.168.101.60:8083';
 
 module.exports = {
-	'/user/indexHotSeasonData.do' : proxy_addr,
+	
+	'/user/indexMainData.do' : proxy_addr,					//主页主广告
+	
+	'/user/indexHotSeasonData.do' : proxy_addr,				//当季热销
+	'/user/indexInternalhotelData.do' : proxy_addr,			//国内酒店
+	'/user/indexForeignhotelData.do' : proxy_addr,			//国际酒店
+	'/user/indexTicketData.do' : proxy_addr,				//景点门票
+	'/user/indexPromotionalSaleData.do' : proxy_addr,		//促销特卖
+	'/user/indexSellWellMonthData.do' : proxy_addr,			//本月热销
+	
 }
