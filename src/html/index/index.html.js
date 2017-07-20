801e6243d35fd99c49f4a77094813f3324f3c89d
aphy358 <358937572@qq.com>
2017-07-20 22:16:19 +0800

    1

--- src/html/index/index.html.js
+++ src/html/index/index.html.js
@@ -1,22 +1,22 @@
-const banner = require('./modules/banner.ejs');
-const search = require('./modules/search.ejs');
-const hotSales = require('./modules/hotSales.ejs');
-const hotelRecommands = require('./modules/hotelRecommands.ejs');
-const ticket = require('./modules/ticket.ejs');
-const promotion = require('./modules/promotion.ejs');
-const pageContent = require('./index.ejs');
+const banner            = require('./modules/banner.ejs');
+const search            = require('./modules/search.ejs');
+const hotSales          = require('./modules/hotSales.ejs');
+const hotelRecommands   = require('./modules/hotelRecommands.ejs');
+const ticket            = require('./modules/ticket.ejs');
+const promotion         = require('./modules/promotion.ejs');
+const pageContent       = require('./index.ejs');
 
-const staticConfig = require('../../static/staticConfig');
-const layout = require('../../layout/layout');
-const pageTitle = '房掌柜 · 首页';
+const staticConfig      = require('../../static/staticConfig');
+const layout            = require('../../layout/layout');
+const pageTitle         = '房掌柜 · 首页';
 
-const params = {
-    banner: banner({staticConfig}),                     //banner（千万记得参数是一个对象）
-    search: search(),                                   //搜索框
-    hotSales: hotSales({staticConfig}),                 //当季热销
-    hotelRecommands: hotelRecommands({staticConfig}),   //国内酒店、国际酒店
-    ticket: ticket({staticConfig}),                     //景点门票
-    promotion: promotion({staticConfig}),               //促销特卖
+let params = {
+    banner:          banner({staticConfig}),                     //banner（千万记得参数是一个对象）
+    search:          search(),                                   //搜索框
+    hotSales:        hotSales({staticConfig}),                   //当季热销
+    hotelRecommands: hotelRecommands({staticConfig}),            //国内酒店、国际酒店
+    ticket:          ticket({staticConfig}),                     //景点门票
+    promotion:       promotion({staticConfig}),                  //促销特卖
 }
 
 module.exports = layout.run(pageTitle, pageContent(params));
--- src/html/login/login.html.js
+++ src/html/login/login.html.js
@@ -1,6 +1,6 @@
-const pageContent = require('./login.ejs');
-const layout = require('../../layout/layout');
-const staticConfig = require('../../static/staticConfig');
-const pageTitle = '房掌柜 · 登录';
+const pageContent   = require('./login.ejs');
+const layout        = require('../../layout/layout');
+const staticConfig  = require('../../static/staticConfig');
+const pageTitle     = '房掌柜 · 登录';
 
 module.exports = layout.run(pageTitle, pageContent({staticConfig}), 'no_Header_Footer');
