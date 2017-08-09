be1fef52dbe105f09694f7ef5c2a0528e6b272b4
Joker <chenhua385148231@126.com>
2017-08-09 16:41:12 +0800

    0

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-var utils = require('../build/utils')
-require('should')
+var utils = require('../build/utils');
+var add = require('../testing.js');
+require('should');
 var chai = require('chai'),
   expect = chai.expect;
 
-describe.skip('大数相加add方法', function () {
+describe('大数相加add方法', function () {
   it('字符串"42329"加上字符串"21532"等于"63861"', function () {
     add('42329', '21532')
       .should.equal('63861')
@@ -12,6 +13,9 @@ describe.skip('大数相加add方法', function () {
   it('"843529812342341234"加上"236124361425345435"等于"1079654173767686669"', function () {
     add('843529812342341234', '236124361425345435')
       .should.equal('1079654173767686669')
+
+    expect(add('843529812342341234', '236124361425345435'))
+      .to.equal('1079654173767686669')
   })
 })
 
@@ -24,6 +28,7 @@ describe('测试 utils 里面的方法', function(){
     expect(utils.getAllEntries).to.be.exist
     expect(utils.getAllEntries).not.to.be.empty
     expect(utils.getAllEntries).to.deep.equal([ 'index', 'login' ])
+    expect(utils.getAllEntries).to.have.length.above(1)
   })
 
 })
