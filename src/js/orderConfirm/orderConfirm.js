95925b2c045d6b2af58784b49e1d7e2cc489fa41
Joker <chenhua385148231@126.com>
2017-08-29 18:30:36 +0800

    0

--- src/js/orderConfirm/orderConfirm.js
+++ src/js/orderConfirm/orderConfirm.js
@@ -17,7 +17,7 @@ require('./modules/initPage.js').run();
 
 const initActive = require('./modules/initActive.js').run;
 
-//IE9以下和IE9以上的浏览器采用不同方式加载插件
+//IE9以下和IE9以上的浏览器采用不同方式加载插件（日期控件、验证控件）
 if( Util.ltIE9() ){
     Util.loadAsync(['../static/js/datePick/datepickPacked.js', '../static/js/validator/validatorPacked.js'], initActive);
 }else{
