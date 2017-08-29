349c13e8ace6c667027b3a22ded3264aa80c182c
Joker <chenhua385148231@126.com>
2017-08-29 18:05:58 +0800

    1

--- src/common/util.js
+++ src/common/util.js
@@ -59,7 +59,7 @@ function loadAsync(src, callback){
 function queryString(name) {
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
 	var r = window.location.search.substr(1).match(reg);
-	if (r != null) return unescape(r[2]); return null;
+	return r != null ? unescape(r[2]) : null;
 }
 
 
